0ea7ffe5e913f72fd0f5b5fa63cf1b9be45613bd
Sachin <sachinchoudhary1908.com>
2026-06-20 13:15:32 +0530

    commit service with base server config setup

--- order1/src/main/java/com/ecommerce/order1/Order1Application.java
+++ order1/src/main/java/com/ecommerce/order1/Order1Application.java
@@ -2,7 +2,7 @@ package com.ecommerce.order1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+
 
 @SpringBootApplication
 
